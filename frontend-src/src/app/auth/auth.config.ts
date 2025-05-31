25e8a6733286ab0c1e1cb132ad966136a8a40af6
jacobmobley <mobley6@purdue.edu>
2025-05-31 00:01:46 -0500

    Revert "I think auth setup a little"
    This reverts commit e2fe574267c57024a98da45f2617c668ad61f06a.

--- frontend-src/src/app/auth/auth.config.ts
+++
@@ -1,9 +0,0 @@
-import { AuthConfig } from '@auth0/auth0-angular';
-
-export const authConfig: AuthConfig = {
-  domain: 'YOUR_AUTH0_DOMAIN', // e.g., 'dev-xyz123.us.auth0.com'
-  clientId: 'YOUR_CLIENT_ID',
-  authorizationParams: {
-    redirect_uri: window.location.origin
-  }
-}; 
